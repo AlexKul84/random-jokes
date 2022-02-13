688bea76a09a4b05511b3bbda6416aaae01e74e9
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-02-13 10:01:37 +0300

    feat: add translator

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,20 +1,40 @@
-async function getData() {
+const english = document.querySelector('.english');
+const russian = document.querySelector('.russian');
+
+// russian.addEventListener('click', function switchToRussian(event) {
+//     if(event.target.classlist.contains('russian')) {
+    
+//     }
+// })  
+        
+async function getDataRu() {
     const quotesRu = 'data_ru.json';
     const resRu = await fetch(quotesRu);
     const data = await resRu.json();
-
     const btn = document.querySelector('.btn');
-
     btn.addEventListener('click', function changeData() {
         document.getElementById("phrase").textContent = data[Math.floor(Math.random() * 100)].text
     });
-
     showData(data[Math.floor(Math.random() * 100)].text)
-  }
+}
+
+async function getDataEn() {
+    const quotesEn = 'data_en.json';
+    const resEn = await fetch(quotesEn);
+    const data = await resEn.json();
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', function changeData() {
+        document.getElementById("phrase").textContent = data[Math.floor(Math.random() * 100)].text
+    });
+    showData(data[Math.floor(Math.random() * 100)].text)
+}
+
+russian.addEventListener('click', getDataRu)
+english.addEventListener('click', getDataEn)
 
 
 function showData(data) {
     document.getElementById("phrase").textContent = data
 }
 
-getData();
+getDataRu();
